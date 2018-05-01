b38253be5615ecbb330fb4cb78313ec1c3451c4f
hlws2014 <wjie.ruan@gmail.com>
2018-05-01 18:53:58 +0100

    wenjie

--- ExperimentCode/Experiment3_ASCSXU_NNs/calculate_z_i.m
+++
@@ -1,7 +0,0 @@
-function [z_i_1,z_i] = calculate_z_i(sortedYK,K,y_i_w)
-
-z_i_1 = 0.5*(y_i_w(1,2)+sortedYK(1,2))-0.5*K*(y_i_w(1,1)-sortedYK(1,1));
-z_i = 0.5*(y_i_w(1,2)+sortedYK(2,2))-0.5*K*(sortedYK(2,1)-y_i_w(1,1));
-
-end
-
